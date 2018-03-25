5321de42b10640f18058f84e89281c3e480b9da9
antonybaciliosalazar@gmail.com <antonybaciliosalazar@gmail.com>
2018-03-25 23:59:53 +0200

    accueil

---
+++ src/CONTROLLER/controleurAccueil.java
@@ -0,0 +1,39 @@
+package CONTROLLER;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class controleurAccueil {
+
+	@FXML
+    private Label lblTitle;
+
+    @FXML
+    private Label lblBienvenue;
+
+    @FXML
+    private Button btnPendu;
+
+    @FXML
+    private Button btnTaquin;
+
+    @FXML
+    private Button btnTictactoc;
+
+    @FXML
+    void btnPendu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTaquin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTictactoc(ActionEvent event) {
+
+    }
+
+}
